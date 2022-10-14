851f0ff8035ca40b65b9e279bc7ef33477221981
SKATASHIN <katashin0544@gmail.com>
2022-10-14 16:46:18 +0900

    ルート設定まで

--- routes/member.php
+++ routes/member.php
@@ -1,15 +1,37 @@
 <?php
 
-use App\Http\Controllers\User\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\User\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\User\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\User\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\User\Auth\NewPasswordController;
-use App\Http\Controllers\User\Auth\PasswordResetLinkController;
-use App\Http\Controllers\User\Auth\RegisteredUserController;
-use App\Http\Controllers\User\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Member\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Member\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Member\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Member\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Member\Auth\NewPasswordController;
+use App\Http\Controllers\Member\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Member\Auth\RegisteredUserController;
+use App\Http\Controllers\Member\Auth\VerifyEmailController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+//authからコピーしている
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
@@ -54,3 +76,4 @@ Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
 });
+
